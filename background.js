b32473bd407ee2cec0302b79776e6843633c8e82
satish pati <satishpati2006@gmail.com>
2024-10-08 23:41:18 +0530

    Code for bookmarks

---
+++ background.js
@@ -0,0 +1,25 @@
+(() => {
+    let youtubeLeftControls, youtubePlayer;
+    let currentvideo = "";
+
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const {type, value, videoId} = obj;
+
+        if(type == "NEW"){
+            currentVideo = videoId;
+            newVideoLoaded();
+        }
+    });
+
+    const newVideoLoaded = () =>{
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+
+        if (!bookmarkBtnExists){
+            const bookmarkBtn = document.createElement("img");
+
+            bookmarkBtn.src = crome.runtime.getURL("assets/bookmark.png");
+            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+            bookmarkBtn.title = "click to bookmark current timestamp";
+        }
+    }
+})();
